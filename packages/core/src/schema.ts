8841433ecc20013ff99f95d6ef2827abb1be6d22
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2024-01-03 22:46:43 -0300

    refactor: fullQualifiedKeys should return the key and the object path
    this will allow the loaders to recreate exacly the same object used to
    create the key

--- packages/core/src/schema.ts
+++ packages/core/src/schema.ts
@@ -21,10 +21,40 @@ export type SchemaValidationResult<Schema, SchemaType extends SchemaTypeProvider
   | { success: true; data: InferOutput<SchemaType, Schema> }
   | { success: false; error: InferError<SchemaType> };
 
+export type FQLN = { key: string; path: string[] };
+
 export type SchemaProvider<SchemaType extends SchemaTypeProvider = SchemaTypeProvider> = {
   validate: <TSchema extends InferBaseSchema<SchemaType>>(
     schema: TSchema,
     input: unknown,
   ) => SchemaValidationResult<TSchema, SchemaType>;
-  fullQualifiedKeys: <TSchema extends InferBaseSchema<SchemaType>>(schema: TSchema) => string[];
+
+  /**
+   * Returns an array with the FQLN key and the object path.
+   * We use this key to load from environment variables or
+   * any other Key/value configuration source.
+   *
+   * The path is the object path to the value in the schema.
+   * For example, if we have the following schema:
+   *
+   * ```ts
+   * const schema = z.object({
+   *  db: z.object({ url: z.string() }),
+   *  port: z.number(),
+   *  NODE_ENV: z.string(),
+   *  });
+   *  ```
+   *
+   *  The result will be:
+   *  ```ts
+   *  [
+   *    {key: 'DB_URL', path: ['db', 'url']},
+   *    {key: 'PORT', path: ['port']},
+   *    {key: 'NODE_ENV', path: ['NODE_ENV']}
+   *  ]
+   *  ```
+   *
+   * @param schema The schema to load.
+   */
+  fullQualifiedKeys: <TSchema extends InferBaseSchema<SchemaType>>(schema: TSchema) => FQLN[];
 };
