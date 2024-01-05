5c31adfe0221780183abbabd235c2a14620f1e6b
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2024-01-04 21:54:43 -0300

    refactor: cleanup not used imports and types

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,5 +1,4 @@
-import * as zodProvider from './adapters/zod';
-import { InferError, SchemaTypeProvider, FQLN } from './schema';
+import { FQLN } from './schema';
 
 export type Simplify<T> = {
   [P in keyof T]: T[P];
@@ -9,17 +8,6 @@ export interface Dict<T> {
   [key: string]: T | undefined;
 }
 
-export type RawObject = Dict<RawObject | string>;
-
 export interface ConfigLoader {
   load(fqlns: FQLN[]): Promise<unknown>;
-}
-
-type DefaultSchemaTypeProvider = zodProvider.ZodSchemaTypeProvider;
-
-export interface ConfigOptions<TypeProvider extends SchemaTypeProvider = DefaultSchemaTypeProvider> {
-  schema: TypeProvider['schema'];
-  onValidationError?: (error: InferError<TypeProvider>, dataLoaded: unknown) => never;
-  onDataLoaderError?: (error: Error) => never;
-  loaders: ConfigLoader[];
 }
